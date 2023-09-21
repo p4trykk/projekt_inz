925a131ab10e611de4bf9f7d8646cf3e22a0b666
p4trykk <pklytta1@gmail.com>
2023-09-21 11:56:31 +0200

    add new noises to library
    add new noises to library (green noise) + custom tkinter changes

--- project_Patryk_Klytta.py
+++ project_Patryk_Klytta.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.io.wavfile as wav
-from scipy.signal import butter, lfilter
+from scipy.signal import butter, lfilter, buttord, filtfilt
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -18,7 +18,9 @@ from scipy.signal import hilbert
 import wavio
 from PIL import Image, ImageTk
 from scipy.io.wavfile import write
-from scipy.signal import lfilter
+from scipy.stats import norm
+from numpy import int16
+import tkinter.font as tkFont
 
 pygame.mixer.init()
 
@@ -141,6 +143,63 @@ pygame.mixer.init()
 # plt.savefig(spectrum_image_path)
 
 
+#Generowanie Green noises: (jednorazowo)
+# def turn_green(signal, samp_rate):
+#     # start and stop of green noise range
+#     left = 1612 # Hz
+#     right = 2919 # Hz
+
+#     nyquist = (samp_rate/2)
+#     left_pass  = 1.1*left/nyquist
+#     left_stop  = 0.9*left/nyquist
+#     right_pass = 0.9*right/nyquist
+#     right_stop = 1.1*right/nyquist
+
+#     (N, Wn) = buttord(wp=[left_pass, right_pass],
+#                       ws=[left_stop, right_stop],
+#                       gpass=2, gstop=30, analog=0)
+#     (b, a) = butter(N, Wn, btype='band', analog=0, output='ba')
+#     return filtfilt(b, a, signal)
+
+# def to_integer(signal):
+#     # Take samples in [-1, 1] and scale to 16-bit integers,
+#     # values between -2^15 and 2^15 - 1.
+#     signal /= max(signal)
+#     return int16(signal*(2**15 - 1))
+
+# N = 48000 # samples per second
+
+# duration = 60.0 # seconds
+# num_samples = int(N * duration)
+
+# white_noise= norm.rvs(0, 1, num_samples) 
+# green = turn_green(white_noise, N)
+
+# # Zapisz szum do pliku .wav
+# output_path = "C:\\Users\\pklyt\\Desktop\\studia\\inz\\szumy\\green_noise.wav"
+# write(output_path, N, to_integer(green))
+
+# # Obliczenie widma
+# Y = np.fft.fft(green)
+# frequencies = np.fft.fftfreq(len(Y), 1.0/N)
+# positive_freq_idxs = np.where(frequencies > 0)
+# frequencies = frequencies[positive_freq_idxs]
+# magnitude = np.abs(Y[positive_freq_idxs])
+
+# # Wykres widma
+# plt.figure()
+# plt.plot(frequencies, 20 * np.log10(magnitude))
+# plt.xscale('log')
+# plt.title("Widmo sygnału szumu zielonego")
+# plt.xlabel("Częstotliwość [Hz]")
+# plt.ylabel("Amplituda [dB]")
+# plt.xlim((1500, 3000))
+
+# # Zapisz wykres do pliku .png
+# spectrum_image_path = "C:\\Users\\pklyt\\Desktop\\studia\\inz\\szumy\\green_noise_spectrum.png"
+# plt.savefig(spectrum_image_path)
+
+
 def butter_bandstop_filter(data, lowcut, highcut, fs, order=6):
     nyq = 0.5 * fs
     low = lowcut / nyq
@@ -180,6 +239,7 @@ DARK_MODE = {
     "text": "#FFFFFF",
 }
 
+
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -193,9 +253,12 @@ class Application(tk.Tk):
         self.file_for_menu_1 = "C:\\Users\\pklyt\\Desktop\\studia\\inz\\szumy\\brown_noise.wav"
         self.file_for_menu_2 = "C:\\Users\\pklyt\\Desktop\\studia\\inz\\szumy\\white_noise.wav"
         self.file_for_menu_3 = "C:\\Users\\pklyt\\Desktop\\studia\\inz\\szumy\\pink_noise.wav"
+        self.file_for_menu_4 = "C:\\Users\\pklyt\\Desktop\\studia\\inz\\szumy\\green_noise.wav"
         self.file_for_menu_1_widmo = "C:\\Users\\pklyt\\Desktop\\studia\\inz\\szumy\\brown_noise_spectrum.png"
         self.file_for_menu_2_widmo = "C:\\Users\\pklyt\\Desktop\\studia\\inz\\szumy\\white_noise_spectrum.png"
         self.file_for_menu_3_widmo = "C:\\Users\\pklyt\\Desktop\\studia\\inz\\szumy\\pink_noise_spectrum.png"
+        self.file_for_menu_4_widmo = "C:\\Users\\pklyt\\Desktop\\studia\\inz\\szumy\\green_noise_spectrum.png"
+    
 
     def create_widgets(self):
         self.play_icon = PhotoImage(file="C:\\Users\\pklyt\\Desktop\\studia\\inz\\play_icon.png")  
@@ -211,11 +274,11 @@ class Application(tk.Tk):
         self.app_name_label.grid(row=0, column=1, pady=(0,5), sticky="w")
 
         self.input_button = ctk.CTkButton(self, text="Select input file", command=self.select_input, 
-                                    corner_radius=10)
+                                    corner_radius=10, hover_color='#100d33')
         self.input_button.grid(row=0, column=2, padx=5, pady=(0,5), sticky="e")  
 
         self.filter_button = ctk.CTkButton(self, text="Filter", command=self.filter, 
-                                    corner_radius=10)
+                                    corner_radius=10, hover_color='#100d33')
         self.filter_button.grid(row=0, column=3, padx=(5, 10), pady=(0,5), sticky="w")
 
         self.play_button = tk.Button(self, image=self.play_icon, command=self.play, 
@@ -246,14 +309,17 @@ class Application(tk.Tk):
 
         menu_frame.grid(row=2, column=0, sticky="nsew", padx=(10,0))
 
-        self.menu_button_1 = ctk.CTkButton(menu_frame, text="Brown noise", command=self.menu_command_1, corner_radius=10, width=30)
+        self.menu_button_1 = ctk.CTkButton(menu_frame, text="Brown noise", command=self.menu_command_1, corner_radius=10, width=30, hover_color='#100d33')
         self.menu_button_1.pack(fill="x", pady=(5,12))  
 
-        self.menu_button_2 = ctk.CTkButton(menu_frame, text="White noise", command=self.menu_command_2, corner_radius=10, width=30)
+        self.menu_button_2 = ctk.CTkButton(menu_frame, text="White noise", command=self.menu_command_2, corner_radius=10, width=30, hover_color='#100d33')
         self.menu_button_2.pack(fill="x", pady=(5,12))
 
-        self.menu_button_3 = ctk.CTkButton(menu_frame, text="Pink noise", command=self.menu_command_3, corner_radius=10, width=30)
+        self.menu_button_3 = ctk.CTkButton(menu_frame, text="Pink noise", command=self.menu_command_3, corner_radius=10, width=30, hover_color='#100d33')
         self.menu_button_3.pack(fill="x", pady=(5,12))
+
+        self.menu_button_4 = ctk.CTkButton(menu_frame, text="Green noise", command=self.menu_command_4, corner_radius=10, width=30, hover_color='#100d33')
+        self.menu_button_4.pack(fill="x", pady=(5,12))
 
         if not self.info_text:
             self.info_text = tk.Label(self, bg="#F0F0F0", wraplength=300, justify='left', anchor='nw')
@@ -414,6 +480,26 @@ class Application(tk.Tk):
         self.after(100, self.update_progressbar, duration_ms)
         self.info_text.grid()
         self.info_text.config(text="Tutaj umieść informacje na temat szumu różowego.")
+
+    def menu_command_4(self):
+        self.output_file = self.file_for_menu_4
+        spectrum_path = self.file_for_menu_4_widmo
+        imgWidmo = Image.open(spectrum_path)
+        # imgWidmo = ImageTk.PhotoImage(imgWidmo)
+        for ax in self.figure.axes:
+            ax.remove()
+        ax = self.figure.add_axes([0,0,1,1])
+        ax.imshow(imgWidmo)
+        ax.axis('off')
+        self.canvas.draw()
+        pygame.mixer.music.load(self.output_file)
+        pygame.mixer.music.play()
+        duration_ms = pygame.mixer.Sound(self.output_file).get_length() * 1000  
+        self.progressbar["maximum"] = duration_ms 
+        self.progressbar["value"] = 0
+        self.after(100, self.update_progressbar, duration_ms)
+        self.info_text.grid()
+        self.info_text.config(text="Tutaj umieść informacje na temat szumu zielonego.")
 
 if __name__ == "__main__":
     app = Application()
